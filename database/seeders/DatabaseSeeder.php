37ed8d934552315323c73f42ba34a3b82124b039
rifky290301 <rifky290301@gmail.com>
2021-06-16 21:34:31 +0700

    initial
    awali dengan bismillah

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Auth\Events\Registered;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roleSuper = Role::create([
+            'name' => 'Super admin',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $roleBPH = Role::create([
+            'name' => 'BPH',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $roleSantri = Role::create([
+            'name' => 'Santri',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        Permission::create([
+            'name' => 'create user',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        Permission::create([
+            'name' => 'create role',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        Permission::create([
+            'name' => 'create permission',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        Permission::create([
+            'name' => 'membuat artikel',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        Permission::create([
+            'name' => 'membuat berita',
+            'guard_name' => 'web',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        $roleSuper->givePermissionTo(['create user', 'create permission', 'create role', 'membuat berita', 'membuat artikel']);
+        // $roleSuper->givePermissionTo('create permission');
+        // $roleSuper->givePermissionTo('create role');
+
+        $roleBPH->givePermissionTo('create user');
+
+        $roleSantri->givePermissionTo('membuat artikel');
+
+        $superAdmin = User::create([
+            'name' => 'Superadmin akun',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('superadmin123'),
+            'remember_token' => \Str::random(60),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $superAdmin->assignRole('Super admin');
+        event(new Registered($superAdmin));
+
+        $roleBPH = User::create([
+            'name' => 'akun bph',
+            'email' => 'bph@gmail.com',
+            'password' => bcrypt('bph123'),
+            'remember_token' => \Str::random(60),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $roleBPH->assignRole('BPH');
+        event(new Registered($roleBPH));
+
+        $roleSantri = User::create([
+            'name' => 'akun santri',
+            'email' => 'santri@gmail.com',
+            'password' => bcrypt('santri123'),
+            'remember_token' => \Str::random(60),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $roleBPH->assignRole('Santri');
+        event(new Registered($roleBPH));
+    }
+}
